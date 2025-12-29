11cff47a0d79a08fe22521b2fbe595fda0860853
Isaac A. H. F. de Melo <126210328+IsaacDeMelo@users.noreply.github.com>
2025-12-29 11:39:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const activeBattles = {}; 
 const onlineBattles = {}; 
